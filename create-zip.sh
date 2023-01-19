159cc1a29b36b9e3de10c3f6c73a6befb0807a80
Jonh Wendell <jwendell@redhat.com>
2023-01-19 14:19:32 -0500

    Only works in GNOME 43

--- create-zip.sh
+++ create-zip.sh
@@ -6,7 +6,7 @@ ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/." && pwd)"
 (
 cd "${ROOTDIR}"
 rm -f "${OUTPUT}"
-zip -r -q "${OUTPUT}" . --exclude "*.git*"
+zip -r -q "${OUTPUT}" . --exclude "*.git*" --exclude "create-zip.sh"
 )
 
 echo "Created ${ROOTDIR}/${OUTPUT}"
